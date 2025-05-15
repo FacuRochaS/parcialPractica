90c8c9ca93c55a3542025d0094a8da402b92cd8e
Facu <facurochaseret@gmail.com>
2025-05-15 17:29:06 -0300

    hola

---
+++ src/test/java/ar/edu/utn/frc/tup/lciii/DeckTest.java
@@ -0,0 +1,38 @@
+package ar.edu.utn.frc.tup.lciii;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @Test
+    void createDeckTest() {
+        // TODO: Crear un test que valide que el mazo se crea con 40 cartas,
+        //  que no se incluyen los 8 y 9.
+        //  Validar que todas las cartas de un mazo de 40 cartas estén presentes.
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void takeCardTest() {
+        // TODO: Crear un test que valide que al tomar una carta del mazo,
+        //  la cantidad de cartas en el mazo disminuye en 1 y que la carta tomada
+        //  es la que se esperaba; es decir la que está al tope de la pila.
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void isEmptyTest() {
+        // TODO: Crear un test que valide que el mazo está vacío cuando no tiene cartas
+        //  y que no está vacío cuando tiene al menos una carta.
+        fail("Not implemented yet");
+    }
+
+    @Test
+    void shuffleDeckTest() {
+        // TODO: Crear un test que valide que al mezclar el mazo, las cartas no están en el mismo orden
+        //  que al crear el mazo.
+        fail("Not implemented yet");
+    }
+}
